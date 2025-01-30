404ad1a6bfdd937788f2d83d18791cf3ac4ab391
NiebianskoLicy <Robek0410@gmail.com>
2025-01-30 09:01:46 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
+from email.mime import image
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageDraw, ImageFont
-
-def open_image():
-    pass
 
 window = Tk()
 window.title("Image Watermarking")
 window.minsize(width=500, height=350)
 
 watermark_label = Label(window, text="Image for Watermark", font=("Arial", 18, "bold"))
-watermark_label.place(x=110, y=10)
+watermark_label.place(x=120, y=10)
 
 sel_img_label = Label(window, text="Write text to watermark", font=("Arial", 12))
-sel_img_label.place(x=160, y=60)
+sel_img_label.place(x=170, y=60)
 
 text = Text(window, height=8, width=35, font=("Arial", 12))
 text.focus()
 text.place(x=85, y=90)
-
-def browse_files():
-    filename = filedialog.askopenfilename(initialdir="/",
-                                          title="Select a File",
-                                          filetypes=(
-                                              ("Image Files", "*.jpg *.png *.gif *.img"),
-                                              ("All Files", "*.*")
-                                          ))
-    label_file_explorer.configure(text="File Opened: " + filename)
 
 label_file_explorer = Label(window,
                             text = "File Explorer using Tkinter",
@@ -34,10 +23,31 @@ label_file_explorer = Label(window,
                             fg = "blue")
 label_file_explorer.place(x=0, y=250)
 
-select_button = Button(window, text="Select Image", command=browse_files)
-select_button.place(x=83, y=310)
+filename = filedialog.askopenfilename(initialdir="/",
+                                      title="Select a File",
+                                      filetypes=(
+                                          ("Image Files", "*.jpg *.png *.gif *.img"),
+                                          ("All Files", "*.*")
+                                      ))
+label_file_explorer.configure(text="File Opened: " + filename)
 
-watermark_button = Button(window, text="Add Watermark", command=browse_files)
-watermark_button.place(x=310, y=310)
+def watermark_image():
+    # opening image to work with
+    opened_image = Image.open(filename)
+    # selecting width and height at image resolution
+    image.width, image.height = opened_image.size
+    draw = ImageDraw.Draw(opened_image)
+
+    font_size = int(opened_image.width / 8)
+    font = ImageFont.truetype("arial.ttf", font_size)
+
+    x,y = int(opened_image.width / 2), int(opened_image.height / 2)
+
+    draw.text((x, y), text.get("1.0", END), font=font, fill="red")
+    opened_image.show()
+
+
+select_button = Button(window, text="Select Image", command=watermark_image)
+select_button.place(x=215, y=310)
 
 window.mainloop()
